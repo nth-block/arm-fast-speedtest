62d6717d2ad8c9029959e5152d83598251bc1f33
nth-block <20584285+nth-block@users.noreply.github.com>
2020-11-28 00:14:40 +0530

    Adding support for EventHub messaging.Refactor to using module imports rather than file read.

--- speedtest-code/fast-speedtest-api.js
+++ speedtest-code/fast-speedtest-api.js
@@ -1,42 +1,47 @@
 // npm install --global fast-speedtest-api
+// npm install --save @azure/eventhubs
 
 const FastSpeedtest = require("fast-speedtest-api");
 const fs = require('fs');
+const {
+    EventHubProducerClient
+} = require('@azure/event-hubs');
+const EVENT_HUB_CONFIG = require('./event-hub-info');
+const TOKEN = require('./token');
 
-
-
-function getToken()
-{
-    let fastToken = fs.readFileSync('/opt/token.txt','utf-8').trim();
-    return fastToken;
-}
-
-function getFastSpeedTest(fastToken)
-{
+function getFastSpeedTest() {
     //console.log(`${(new Date()).toISOString()}: Getting Internet speed...`);
     let speedtest = new FastSpeedtest({
-    token: fastToken, // required
-    verbose: false, // default: false
-    timeout: 10000, // default: 5000
-    https: true, // default: true
-    urlCount: 5, // default: 5
-    bufferSize: 8, // default: 8
-    unit: FastSpeedtest.UNITS.Mbps // default: Bps
+        token: TOKEN, // required
+        verbose: false, // default: false
+        timeout: 10000, // default: 5000
+        https: true, // default: true
+        urlCount: 5, // default: 5
+        bufferSize: 8, // default: 8
+        unit: FastSpeedtest.UNITS.Mbps // default: Bps
     });
-     
-    speedtest.getSpeed().then(s => {
-        console.log(JSON.stringify(
-            {
-                "Time": `${(new Date()).toISOString()}`,
-                "Speed (Mbps)": `${s}`
-            }
-        ));
+
+    speedtest.getSpeed().then(async s => {
+
+        let speedReport = {
+            "Time": `${(new Date()).toISOString()}`,
+            "Speed (Mbps)": `${s}`
+        };
+
+        const producer = new EventHubProducerClient(EVENT_HUB_CONFIG.eventHubConnectionString, EVENT_HUB_CONFIG.eventHubName);
+        const batch = await producer.createBatch({
+            partitionId: 0
+        });
+        batch.tryAdd({body: speedReport});
+        await producer.sendBatch(batch);
+        await producer.close();
+        console.log(JSON.stringify(speedReport)); 
     }).catch(e => {
         console.error(e.message);
     });
 }
 
 // Entrypoint of the program
-let fastToken = getToken();
-//console.log(`${(new Date()).toISOString()}: Starting...`);
-setInterval(function(){getFastSpeedTest(fastToken)}, 60*1000);
+setInterval(function(){getFastSpeedTest()}, 60*1000);
+
+// getFastSpeedTest();
